0f490b739ecb31f2cc0d9347becafd6b99e68525
Manuel Friedmacher <development@friedmacher.com>
2020-11-14 10:02:43 +0100

    Update copyright information

--- src/com/moris/sys/login/model/User.java
+++ src/com/moris/sys/login/model/User.java
@@ -1,8 +1,14 @@
-/**
- * @author FRIEDMACHER, Manuel
- * created on: 2020-11-12
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2020 Manuel Friedmacher
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU General Public License v3.0
+ * which accompanies this distribution, and is available at
+ * https://github.com/Friedmacher/MoRIS/blob/main/LICENSE
+ *
+ * Contributors:
+ *     Manuel Friedmacher
+ *******************************************************************************/
 package com.moris.sys.login.model;
 
 import java.util.Date;
